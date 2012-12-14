2394f3cfbb8349a0a49d83594bbd05f54612f1c7
jeffpersonified <jeff.matthew.smith@gmail.com>
2012-12-13 23:54:38 -0800

    add new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonebooth::Application.config.secret_token = '1f5c46ed9eee674461a3b35f1048c6b5d74ade93da3a41e5e6f4c4343352b0c6379fb2a19332ca958c568100fc45f4b7dbbb2256afa5438c5fd448f870c2650d'
