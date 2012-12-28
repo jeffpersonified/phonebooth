6ccfcd760764afe5a6ef3d6ae3d2862139020307
jeffpersonified <jeff.matthew.smith@gmail.com>
2012-12-27 19:50:20 -0500

    add rails photo model and integrate with backbone

--- app/assets/javascripts/views/photos/photos_index.js.coffee
+++ app/assets/javascripts/views/photos/photos_index.js.coffee
@@ -2,6 +2,9 @@ class Phonebooth.Views.PhotosIndex extends Backbone.View
 
   template: JST['photos/index']
 
+  initialize: ->
+    @collection.on('reset', @render, this)
+
   render: ->
-    $(@el).html(@template(photos: "photos go here"))
+    $(@el).html(@template(photos: @collection))
     this
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20121227235837) do
+
+  create_table "photos", :force => true do |t|
+    t.text     "hashie"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
